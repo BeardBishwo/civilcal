05f1bc08f471ca32b916bd05f4d2aa3b9f3348e0
Bishwo <bishwo@example.com>
2026-01-17 17:33:48 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,10 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.php82
-    pkgs.composer
-  ];
+  packages = [ pkgs.php82 pkgs.composer ];
 
   # Sets environment variables in the workspace
   env = {};
